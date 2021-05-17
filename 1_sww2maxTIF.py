bc80c8a9798e1640c1c3374dcfb6c318744f07e5
Petar Milevski <pmilevski@protonmail.com>
2021-05-17 16:10:15 +1000

    Update 1_sww2maxTIF.py
    added blockage

--- 1_sww2maxTIF.py
+++ 1_sww2maxTIF.py
@@ -4,12 +4,10 @@ This script is for ARR2019 ANUGA sww post processing
 
 
 from ARR2019_post_processing import sww2maxTIF
-from ARR2019_config import storms, durations, quantities, data_directory, CellSize, blockages
+from ARR2019_config import storms, durations, quantities, data_directory, CellSize, blockage
    
 for storm in storms:
     for duration in durations:
-		for blockage in blockages:
-			event = str(storm) + '%AEP' + str(duration) + 'm_' + blockage
-			print(event)
-			sww2maxTIF(data_directory + event, CellSize = CellSize)
-
+        event = str(storm) + '%AEP' + str(duration) + 'm_' + blockage
+        print(event)
+        sww2maxTIF(data_directory + event, CellSize = CellSize)
