df1dc15585ce57cf047654385e3ec28470520e3e
Petar Milevski <pmilevski@protonmail.com>
2021-05-17 16:04:03 +1000

    Update 2_critical_duration_patterns.py
    added blockage

--- 2_critical_duration_patterns.py
+++ 2_critical_duration_patterns.py
@@ -7,12 +7,12 @@ import os, glob, os.path
 from os.path import expanduser
 
 from ARR2019_post_processing import post_process, write_ARR_results
-from ARR2019_config import storms, durations, quantities, data_directory, locations
+from ARR2019_config import storms, durations, quantities, data_directory, locations, blockage
 
 for storm in storms:
     for quantity in quantities:        
 
-        points_dict = post_process(durations=durations, locations=locations, storm=storm, quantity=quantity, data_directory=data_directory)
+        points_dict = post_process(durations=durations, locations=locations, storm=storm, quantity=quantity, data_directory=data_directory, blockage=blockage)
         
         # Write critical storms for each location to a file
         outname = str(storm) + '%AEP_critical_' + quantity + '.txt'
