434e15b38ca512eb30980079e268cdc7526905fc
Petar Milevski <pmilevski@protonmail.com>
2021-05-19 08:20:47 +1000

    Update 3_meanTIFs2maxTIF_ARR2019.py
    cleaned code

--- 3_meanTIFs2maxTIF_ARR2019.py
+++ 3_meanTIFs2maxTIF_ARR2019.py
@@ -3,8 +3,6 @@ After running the 2_maxTIFs2meanTIF.py script, you need to run this script
 to create a single maximum TIF file from the mean
 for each duration as per ARR2019 requirements
 
-hacked by Petar Milevski 28 April 2021
-
 """
 
 import shutil
@@ -18,8 +16,8 @@ import os.path
 from os.path import *
 
 #### ENTER DIRECTORY LOCATION HERE ###
-#location = '/models/1%AEP/' # this is critical for the scripts to run, you must put all your files in a folder called files
-location = '/Work/Petar-2021/PyARR-data/1%AEP/' # this is critical for the scripts to run, you must put all your files in a folder called files
+location = '/models/1%AEP/' # this is critical for the scripts to run, you must put all your files in a folder called files
+data_directory = expanduser('~')+location
 ######################################
 
 def meanTIF2maxTIF(fromdir, filepattern='*.tif'):
@@ -37,86 +35,18 @@ def meanTIF2maxTIF(fromdir, filepattern='*.tif'):
     # This assumes that all inputs have the same geotransform since we just copy the first
 
     path_list = fromdir.split(os.sep)   
-    print (path_list)
-    outfile = path_list[-2]+'_'+path_list[-1][0:-5]+'_max.tif'
+    print (fromdir)
+    outfile = path_list[4]+'_'+path_list[5][0:-5]+'_max.tif'
     print ('creating peak of peaks', outfile)
     driver = gdal.GetDriverByName('GTiff')
-    result = driver.CreateCopy(outfile, gdal.Open(filenames[0]))
+    result = driver.CreateCopy(fromdir + '/' + outfile, gdal.Open(filenames[0]))
     result.GetRasterBand(1).WriteArray(maximum)
     result = None
 
-    
-# enter the data directory where the model runs are located
-data_directory = expanduser("~")+location
+create_Dmax = meanTIF2maxTIF(data_directory + 'D_mean')
 
+create_VDmax = meanTIF2maxTIF(data_directory + 'VD_mean')
 
-  
-src_dir = data_directory
-os.chdir(src_dir)
-  
-try:	  
-    os.mkdir('D_mean')   
-    os.mkdir('VD_mean')
-    os.mkdir('V_mean')
-    os.mkdir('WL_mean')
-except:
-    pass
+create_Vmax = meanTIF2maxTIF( data_directory + 'V_mean')
 
-dest_dir_d = data_directory+'D_mean'
-dest_dir_vd = data_directory+'VD_mean'
-dest_dir_v = data_directory+'V_mean'
-dest_dir_wl = data_directory+'WL_mean'
-
-print (dest_dir_d)
-print (dest_dir_vd)
-print (dest_dir_v)
-print (dest_dir_wl)
-
-for fname in glob.glob(os.path.join(src_dir,"*_D_mean.tif")):
-    shutil.move(fname,dest_dir_d) 
-
-
-for fname in glob.glob(os.path.join(src_dir,"*_VD_mean.tif")):
-    shutil.move(fname,dest_dir_vd)
-
-
-for fname in glob.glob(os.path.join(src_dir,"*_V_mean.tif")):
-    shutil.move(fname,dest_dir_v)        
-
-
-for fname in glob.glob(os.path.join(src_dir,"*_WL_mean.tif")):
-    shutil.move(fname,dest_dir_wl)
-
-
-
-
-
-try: 
-    fromdir = dest_dir_d
-    check_polys = meanTIF2maxTIF(fromdir)
-except:
-    pass
-
-try: 
-    fromdir = dest_dir_vd
-    check_polys = meanTIF2maxTIF(fromdir)
-except:
-    pass
-
-try: 
-    fromdir = dest_dir_v
-    check_polys = meanTIF2maxTIF(fromdir)
-except:
-    pass
-
-try: 
-    fromdir = dest_dir_wl
-    check_polys = meanTIF2maxTIF(fromdir)
-except:
-    pass
-
-
-# delete all crap xml files
-for filename in listdir(data_directory):
-    if filename.endswith('.xml'):
-        os.remove(data_directory  + '/' +  filename)
+create_WLmax = meanTIF2maxTIF(data_directory + 'WL_mean')
