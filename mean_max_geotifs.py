3a881c12742970f6b2c47d0b290caba2e7627f36
Petar Milevski <19338934+PetarMilevski@users.noreply.github.com>
2021-05-28 15:27:31 +1000

    Update mean_max_geotifs.py

--- mean_max_geotifs.py
+++ mean_max_geotifs.py
@@ -2,7 +2,8 @@
 After running the 1_sww2maxTIFs.py script, you need to run this script
 
 This script will open a directory of maximum TIF files and calculate
-the mean for that duration as per ARR2019 requirements
+the mean for each duration (from the 10 patterns) and then it will
+create the peak of peaks from the means as per ARR2019 requirements
 
 by Petar Milevski - 2021
 
@@ -17,28 +18,29 @@ import os
 import glob
 import os.path
 from os.path import *
-from ARR2019_config import storms, durations, quantities, data_directory, CellSize, blockage
+from ARR2019_config import storms, durations, quantities, data_directory, CellSize, blockage, proc_directory
 
-
-def maxTIF2meanTIF(directory, filepattern='*.tif'):
-    pattern = os.path.join(directory, filepattern)
-    directory = glob.glob(pattern) 
+# create mean from the ten ARR2019 patterns
+def maxTIF2meanTIF(filenames, filepattern='*.tif'):
+    pattern = os.path.join(filenames, filepattern)
+    filenames = glob.glob(pattern) 
 
     res = []
-    for filename in directory:
+    for filename in filenames:
         ds = gdal.Open(filename)
         res.append(ds.GetRasterBand(1).ReadAsArray()) # We assume that all rasters has a single band
     stacked = np.dstack(res) # We assume that all rasters have the same dimensions
     mean = np.mean(stacked, axis=-1)
 
     path_list = fromdir.split(os.sep) 
-    outfile = path_list[5]+'_'+path_list[6]+'_mean.tif'    
+    outfile = event+'_'+quantity+'_mean.tif'    
     print ('Creating mean: ', outfile)
     driver = gdal.GetDriverByName('GTiff')
-    result = driver.CreateCopy(data_directory+outfile, gdal.Open(directory[0]))
+    result = driver.CreateCopy(data_directory+outfile, gdal.Open(filenames[0]))
     result.GetRasterBand(1).WriteArray(mean)
     result = None    
 
+# create peak of peaks from the means
 def meanTIF2maxTIF(fromdir, filepattern='*.tif'):
     pattern = os.path.join(fromdir, filepattern)
     filenames = glob.glob(pattern) 
@@ -50,9 +52,11 @@ def meanTIF2maxTIF(fromdir, filepattern='*.tif'):
     stacked = np.dstack(res) # We assume that all rasters have the same dimensions
     maximum = np.max(stacked, axis=-1)
 
-    path_list = fromdir.split(os.sep)   
-    #print (fromdir)
-    outfile = path_list[4] + '_'+path_list[5][0:-5] + '_max.tif'
+    path_list = fromdir.split(os.sep)
+    storm_list = proc_directory.split(os.sep)
+    #print (storm_list[-2]+'_'+quantity)
+
+    outfile = storm_list[-2] + '_'+quantity + '_max.tif'
     print ('Creating peak of peaks: ', outfile)
     driver = gdal.GetDriverByName('GTiff')
     result = driver.CreateCopy(data_directory + '/' + outfile, gdal.Open(filenames[0]))
@@ -66,54 +70,29 @@ for storm in storms:
 	for duration in durations:
 		for quantity in quantities:
 			event = str(storm) + '%AEP' + str(duration) + 'm_' + blockage
-			fromdir = data_directory + event + '/' + quantity
+			fromdir = proc_directory + event + '/' + quantity
 			#print (fromdir)
 			check_polys = maxTIF2meanTIF(fromdir)
-   
 
-# move created files into respective directories
 os.chdir(data_directory)
 
-D_mean = 'D_mean'
-VD_mean = 'VD_mean'
-V_mean = 'V_mean'
-WL_mean = 'WL_mean'
+for quantity in quantities:
+	new_dir =  proc_directory + quantity + '_mean'
+	os.mkdir(new_dir)
+	for filename in glob.glob(os.path.join(data_directory,'*_' + quantity + '_mean.tif')):
+		shutil.move(filename,proc_directory + quantity + '_mean' + '/')
 
-os.mkdir(D_mean)   
-os.mkdir(VD_mean)
-os.mkdir(V_mean)
-os.mkdir(WL_mean)
+# create peak of peaks and move to new directory
+for storm in storms:
+	new_max_dir = proc_directory + str(storm) + '%AEP_max'
+	os.mkdir(new_max_dir)
 
+for quantity in quantities:
+	create_max = meanTIF2maxTIF(proc_directory + quantity + '_mean')
+	for filename in glob.glob(os.path.join(data_directory,'*_' + quantity + '_max.tif')):
+		shutil.move(filename,new_max_dir)
 
-for filename in glob.glob(os.path.join(data_directory,'*_' + D_mean + '.tif')):
-    shutil.copy(filename,data_directory + D_mean + '/') 
-
-
-for filename in glob.glob(os.path.join(data_directory,'*_' + VD_mean + '.tif')):
-    shutil.copy(filename,data_directory + VD_mean + '/') 
-
-
-for filename in glob.glob(os.path.join(data_directory,'*_' + V_mean + '.tif')):
-    shutil.copy(filename,data_directory + V_mean + '/')      
-
-
-for filename in glob.glob(os.path.join(data_directory,'*_' + WL_mean + '.tif')):
-    shutil.copy(filename,data_directory + WL_mean + '/') 
-    
-
-
-# delete tifs
-for filename in listdir(data_directory):
-    if filename.endswith('.tif'):
-        os.remove(data_directory + filename)
-
-# create peak of peaks
-create_Dmax = meanTIF2maxTIF(data_directory + D_mean)
-create_VDmax = meanTIF2maxTIF(data_directory + VD_mean)
-create_Vmax = meanTIF2maxTIF(data_directory + V_mean)
-create_WLmax = meanTIF2maxTIF(data_directory + WL_mean)
-
-# delete all xml files
+# clean up (delete all xml files)
 for filename in listdir(data_directory):
     if filename.endswith('.xml'):
         os.remove(data_directory + filename)
