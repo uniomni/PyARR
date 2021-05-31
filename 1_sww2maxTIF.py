df45a2874b6a90aae78f840ceec2dfd1db4e16dd
Petar Milevski <19338934+PetarMilevski@users.noreply.github.com>
2021-05-31 14:51:19 +1000

    Update 1_sww2maxTIF.py
    added blockage as a list

--- 1_sww2maxTIF.py
+++ 1_sww2maxTIF.py
@@ -6,10 +6,11 @@ It will convert a collection of sww files to TIF with maximum values of selected
 
 
 from ARR2019_post_processing import sww2maxTIF
-from ARR2019_config import storms, durations, quantities, data_directory, proc_directory, CellSize, blockage
+from ARR2019_config import storms, durations, quantities, data_directory, proc_directory, CellSize, blockages
    
 for storm in storms:
     for duration in durations:
-        event = str(storm) + '%AEP' + str(duration) + 'm_' + blockage
-        print('Processing event', event)
-        sww2maxTIF(data_directory + event, proc_directory + event, CellSize = CellSize)
+        for blockage in blockages:
+            event = str(storm) + '%AEP' + str(duration) + 'm_' + blockage
+            print('Processing event', event)
+            sww2maxTIF(data_directory + event, proc_directory + event, CellSize = CellSize)
