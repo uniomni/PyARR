5c0371b2513d271493e2d541ec2417674d63a4c9
Petar Milevski <19338934+PetarMilevski@users.noreply.github.com>
2021-06-03 11:04:16 +1000

    Update 2_critical_duration_patterns.py

--- 2_critical_duration_patterns.py
+++ 2_critical_duration_patterns.py
@@ -9,16 +9,14 @@ from os.path import expanduser
 from ARR2019_post_processing import post_process, write_ARR_results
 from ARR2019_config import storms, durations, quantities, proc_directory, locations, blockages
 
-
 for storm in storms:
     for quantity in quantities:
-        for blockage in blockages:      
 
-            print('Postprocessing storm %s for quantity %s at all specified locations' % (storm, quantity))
-            points_dict = post_process(durations=durations, locations=locations, storm=storm, quantity=quantity, proc_directory=proc_directory, blockages=blockages)
-            
-            # Write critical storms for each location to a file
-            outname = str(storm) + '%AEP_critical_' + quantity + '.txt'
-            print('Results stored in', outname)
-    
-            write_ARR_results(outname, points_dict)
+        print('Postprocessing storm %s for quantity %s at all specified locations' % (storm, quantity))
+        points_dict = post_process(durations=durations, locations=locations, storm=storm, quantity=quantity, proc_directory=proc_directory, blockages=blockages)
+
+        # Write critical storms for each location to a file
+        outname = str(storm) + '%AEP_critical_' + quantity + '.txt'
+        print('Results stored in', outname)
+
+        write_ARR_results(outname, points_dict)
