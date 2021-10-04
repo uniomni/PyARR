2b45d8182b7935cd72a37d23e53d183a0b3ca173
Petar Milevski <19338934+PetarMilevski@users.noreply.github.com>
2021-10-05 08:56:43 +1100

    Update 1_sww2maxTIF.py
    Hi Ole, as discussed, i have made changes to this script. 
    
    It just reads through a directory that contains all the files and uses the filenames to generate the output TIFs.
    
    Also, I am also using a GUI for the use to point tot he location of the files.
    
    regards

--- 1_sww2maxTIF.py
+++ 1_sww2maxTIF.py
@@ -2,16 +2,18 @@
 
 It will convert a collection of sww files to TIF with maximum values of selected quantities.
 
-Note - this has been tested and works perfectly, dont touch the code
 """
 
-
+import os
+import os.path
+from os.path import *
+from easygui import *
+from os.path import expanduser
 from ARR2019_post_processing import sww2maxTIF
-from ARR2019_config import storms, durations, quantities, data_directory, proc_directory, CellSize, blockages
-   
-for storm in storms:
-    for duration in durations:
-        for blockage in blockages:
-            event = str(storm) + '%AEP' + str(duration) + 'm_' + blockage
-            print('Processing event', event)
-            sww2maxTIF(data_directory + event, proc_directory + event, CellSize = CellSize)
+from ARR2019_config import data_directory, proc_directory, CellSize, quantities, MyTimeStep
+
+
+sub_folders = [name for name in os.listdir(data_directory) if os.path.isdir(os.path.join(data_directory, name))]
+
+for folder in sub_folders:
+    sww2maxTIF(data_directory + folder, proc_directory + 'PyARR-postprocessed-data/' + folder, CellSize = CellSize, MyTimeStep = MyTimeStep)
