a497ce12519df049b2b34c31d61820d65c82cc3a
Petar Milevski <19338934+PetarMilevski@users.noreply.github.com>
2021-10-30 18:45:56 +1100

    Update 2_critical_duration_patterns.py
    Hi Ole, again i have made some amendments to simplify this process. No longer dependent on storm, blockage, duration. Only on quantities.

--- 2_critical_duration_patterns.py
+++ 2_critical_duration_patterns.py
@@ -1,22 +1,19 @@
 """Second script to compute the ARR2019 post processing
 """
 
-        
+from easygui import *       
 from osgeo import gdal
 import os, glob, os.path
 from os.path import expanduser
-
 from ARR2019_post_processing import post_process, write_ARR_results
-from ARR2019_config import storms, durations, quantities, proc_directory, locations, blockages
+from ARR2019_config import data_directory, proc_directory, quantities, locations
 
-for storm in storms:
-    for quantity in quantities:
+for quantity in quantities:
 
-        print('Postprocessing storm %s for quantity %s at all specified locations' % (storm, quantity))
-        points_dict = post_process(durations=durations, locations=locations, storm=storm, quantity=quantity, proc_directory=proc_directory, blockages=blockages)
-
-        # Write critical storms for each location to a file
-        outname = str(storm) + '%AEP_critical_' + quantity + '.txt'
-        print('Results stored in', outname)
-
-        write_ARR_results(outname, points_dict)
+    points_dict = post_process(locations=locations, quantity=quantity, proc_directory=proc_directory)
+    
+    # Write critical storms for each location to a file
+    outname = quantity + '.txt'
+    print('Results stored in', outname)
+    
+    write_ARR_results(outname, points_dict)
