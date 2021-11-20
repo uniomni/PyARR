833a906b73179f2d159bd769d182a2689191aa7e
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2021-11-20 17:21:01 +1100

    Added third script

---
+++ 3_peakofpeaks.py
@@ -0,0 +1,41 @@
+""" 
+After running the 1_sww2maxTIFs.py script, you need to run this script
+
+This script will open a directory of maximum TIF files and calculate
+the mean for each duration (from the 10 patterns) and then it will
+create the peak of peaks from the means or medians as per ARR2019 requirements
+
+by Petar Milevski - 2021
+
+"""
+
+import os
+import glob
+import shutil
+from os.path import *
+from ARR2019_config import storm, mode, quantities, root_directory
+from ARR2019_post_processing import maxTIF2meanTIF, meanTIF2maxTIF
+
+TIF_directory = os.path.join(root_directory, 'TIFS')
+MEAN_directory = os.path.join(root_directory, 'MEANS')
+PEAK_directory = os.path.join(root_directory, 'PEAKS')
+sub_folders = [name for name in os.listdir(TIF_directory) if os.path.isdir(os.path.join(TIF_directory, name))]
+
+for folder in sub_folders:
+    for quantity in quantities:
+        fromdir = os.path.join(TIF_directory, folder, quantity)
+        output_filename = folder + '_' + quantity + '_' + mode + '.tif' 
+
+        if os.path.isfile(os.path.join(MEAN_directory, output_filename)):
+            print('Already computed', output_filename)
+        else: 
+            print('Computing', output_filename)
+        
+            maxTIF2meanTIF(fromdir, MEAN_directory, output_filename, mode=mode)
+
+print('Computing Peak of Peaks!')
+for quantity in quantities:
+    print(quantity)
+    meanTIF2maxTIF(MEAN_directory, PEAK_directory, quantity, mode=mode)
+    
+
