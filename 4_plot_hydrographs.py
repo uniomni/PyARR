2f2d25d412c40f3fa1fc087e6beeeb0859df98ce
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2021-11-23 10:11:45 +1100

    Made plots stored in PLOTS folder under the common data area

--- 4_plot_hydrographs.py
+++ 4_plot_hydrographs.py
@@ -5,8 +5,8 @@ from ARR2019_config import root_directory
 from ARR2019_post_processing import plot_hydrographs
 
 # enter location poly here 
-polyline = [(299167.162,6180397.967),(299232.924,6180475.358)] #this one is for Brooks Ck outlet
-#polyline = [(306632.382,6186448.055),(306680.364,6186443.441)] #this one is for Wollongong City outlet
+#polyline = [(299167.162,6180397.967),(299232.924,6180475.358)] #this one is for Brooks Ck outlet
+polyline = [(306632.382,6186448.055),(306680.364,6186443.441)] #this one is for Wollongong City outlet
 
 data_directory = os.path.join(root_directory, 'SWW')
 sub_folders = [name for name in os.listdir(data_directory) if os.path.isdir(os.path.join(data_directory, name))]
@@ -25,15 +25,20 @@ sub_folders = [name for name in os.listdir(data_directory) if os.path.isdir(os.p
 import matplotlib.pyplot as plt
 from anuga.shallow_water.sww_interrogate import get_flow_through_cross_section
 
+destdir = os.path.join(root_directory, 'PLOTS')
+print(f'Making {destdir}')
+os.makedirs(destdir, exist_ok=True)
 
 for folder in sub_folders:
     
     fromdir = os.path.join(root_directory, 'SWW', folder)
-    destdir = os.path.join(root_directory, 'PLOTS', folder)    
     
     pattern = os.path.join(fromdir, '*.sww')
     filenames = glob.glob(pattern) 
-    print (folder)
+
+    target = os.path.join(destdir, folder + '.png')
+    print(f'Saving to {target}')
+    
     
     for filename in filenames: 
 
@@ -50,7 +55,7 @@ for folder in sub_folders:
         plt.xlabel('Time (hours)')
         plt.ylabel('Flows (m3/sec)')
     
-    plt.savefig(folder + '.png')
+    plt.savefig(target)
       
     print('finished plotting: ', folder)
         #plt.show()
--- ARR2019_config.py
+++ ARR2019_config.py
@@ -10,8 +10,8 @@ from os.path import expanduser
 from easygui import *
   
 # Set data directory
-root_directory = diropenbox('Select data directory', default=expanduser('~'))
-#root_directory = '/home/ro/Work/Petar-2021/PyARR-data/1%AEP'
+#root_directory = diropenbox('Select data directory', default=expanduser('~'))
+root_directory = '/home/ro/Work/Petar-2021/PyARR-data/1%AEP'
 #root_directory = '/media/dpm/TOSHIBA EXT/BrooksCk_ARR2019_ANUGA_Results/Results/1%AEP'
 
 
