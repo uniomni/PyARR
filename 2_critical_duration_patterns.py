5802037c456a21fa473a5584c7c1b9ed7a8232a4
Petar Milevski <pmilevski@gmail.com>
2022-03-16 12:10:38 +1100

    Update 2_critical_duration_patterns.py

--- 2_critical_duration_patterns.py
+++ 2_critical_duration_patterns.py
@@ -8,8 +8,7 @@ from ARR2019_post_processing import post_process, write_ARR_results
 data_directory = os.path.join(root_directory, 'TIFS')
 
 for quantity in quantities:
-    points_dict = post_process(locations=locations, quantity=quantity, data_directory=data_directory, mode=mode)    
-    
-    outname = os.path.join(root_directory, storm) + '_' + quantity + '.txt'
+    points_dict = post_process(locations = locations, quantity = quantity, data_directory = data_directory, mode = mode)       
+    outname = os.path.join(root_directory, storm) + '_' + quantity + '_' + mode + '.txt'
     print('Results stored in', outname)
     write_ARR_results(outname, points_dict, mode)
