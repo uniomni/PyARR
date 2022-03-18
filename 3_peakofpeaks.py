9526493642233d385206bcdbd93aa3089e6f5cfd
Petar Milevski <pmilevski@gmail.com>
2022-03-18 11:37:19 +1100

    Update 3_peakofpeaks.py
    added already computed check on peak pf peaks calc

--- 3_peakofpeaks.py
+++ 3_peakofpeaks.py
@@ -2,8 +2,8 @@
 After running the 1_sww2maxTIFs.py script, you need to run this script
 
 This script will open a directory of maximum TIF files and calculate
-the mean for each duration (from the 10 patterns) and then it will
-create the peak of peaks from the means or medians as per ARR2019 requirements
+the mean, median and maximum for each duration (from the 10 patterns) 
+and then it will create the peak of peaks from the as per ARR2019 requirements
 
 by Petar Milevski and Ole Nielsen - 2021
 
@@ -22,7 +22,8 @@ PEAK_directory = os.path.join(root_directory, 'PEAKS')
 sub_folders = [name for name in os.listdir(TIF_directory) if os.path.isdir(os.path.join(TIF_directory, name))]
 
 for mode in mode:
-	
+    print ('Extracting: ', mode)
+    	
     for folder in sub_folders:
         for quantity in quantities:
             fromdir = os.path.join(TIF_directory, folder, quantity)
@@ -34,12 +35,13 @@ for mode in mode:
             else: 
                 print('Computing', output_filename)
             
-                maxTIF2meanTIF(fromdir, destdir, output_filename, mode=mode)
-                
-                        
-                
-    print('Computing Peak of Peaks!')
-    for quantity in quantities:
-    	# FIXME need a check here to skip already created peak of peaks
-        print(quantity)
-        meanTIF2maxTIF(MEAN_directory + '/' + quantity + mode, PEAK_directory, quantity, mode=mode)
+                maxTIF2meanTIF(fromdir, destdir, output_filename, mode = mode)
+               
+    for quantity in quantities:   	
+        output_filename = quantity + '_' + mode + '_peakofpeaks.tif' 
+        
+        if os.path.isfile(os.path.join(PEAK_directory, output_filename)):
+            print('Already computed', output_filename)
+        else: 
+            print('Computing', output_filename)
+            meanTIF2maxTIF(MEAN_directory + '/' + quantity + mode, PEAK_directory, quantity, mode = mode)
