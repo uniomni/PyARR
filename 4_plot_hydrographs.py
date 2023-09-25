36e0630c3ea110f0da10f7a344743fa050fe5bf3
Petar Milevski <pmilevski@gmail.com>
2023-09-26 09:35:49 +1000

    Add files via upload
    added peak flow to title of plot

--- 4_plot_hydrographs.py
+++ 4_plot_hydrographs.py
@@ -10,7 +10,9 @@ from anuga.caching import cache
 
 # Enter location poly here 
 #polyline = [(299167.162,6180397.967),(299232.924,6180475.358)] #this one is for Brooks Ck outlet
-polyline = [(306632.382,6186448.055),(306680.364,6186443.441)] #this one is for Wollongong City outlet
+#polyline = [(306632.382,6186448.055),(306680.364,6186443.441)] #this one is for Wollongong City outlet
+polyline = [(314029.861,6214403.147),(314011.461,6214459.153)] #Walker St Helensburgh
+#polyline = [(314092.661,6214427.176),(314070.568,6214428.145)] #Buldo Lane Helensburgh
 
 # Directories
 data_directory = os.path.join(root_directory, 'SWW')
@@ -78,7 +80,8 @@ for folder in sub_folders:
                      dependencies=filename,
                      verbose=False)
         plt.plot(t/3600., Q, label=label)
-        plt.legend(loc='best')            
+        plt.legend(loc='best')    
+        plt.title('Qmax = %.3f m3/s' %(max(Q)))        
         
     plt.savefig(target)
     print(f'finished plotting {folder} in {(time.time() - t0):.2f} seconds')
